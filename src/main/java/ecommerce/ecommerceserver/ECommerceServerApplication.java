1ce70073905e0fdb320d7b6d9d488dc6b9508c22
Oğuz Direnç DİNÇ <oguzdirenc@hotmail.com>
2021-02-02 14:55:42 +0300

    First commit

---
+++ src/main/java/ecommerce/ecommerceserver/ECommerceServerApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.ecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ecommerce/ecommerceserver/ECommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.ecommerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
